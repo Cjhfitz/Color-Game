f3eb63fa50fa56c114310d4bedd7031c6a79f103
Conor <cjhfitz@gmail.com>
2020-08-05 01:58:28 -0400

    cleaning code

--- js/ColorGame.js
+++ js/ColorGame.js
@@ -1,61 +1,40 @@
 
 let numSquares = 6;
-let rgbVals = generateRGB(numSquares);
+let rgbVals = generateSquareColor(numSquares);
 let difficulty = "hard";
 let display = document.querySelector("#display");
-
 let h1 = document.querySelector("h1");
-h1.textContent = rgbVals[getRandomInt(rgbVals.length)];
 let squares = document.querySelectorAll(".square");
+let modeButtons = document.querySelectorAll(".mode");
+
+h1.textContent = rgbVals[getRandomInt(rgbVals.length)];
+
 generateSquares();
 makeChoice();
 
-let modeButtons = document.querySelectorAll(".mode");
 
 for(let i = 0; i < modeButtons.length; i++) {
     modeButtons[i].addEventListener("click", function() {
         if(modeButtons[i].textContent.toLowerCase() === "easy") {
             numSquares = 3;
             difficulty = "easy";
+            for(let i =  3; i <= 5; i++) {
+                squares[i].style.background="#343a40";
+            }
         } else if(modeButtons[i].textContent.toLowerCase() === "hard") {
             numSquares = 6;
             difficulty = "hard";
         }
-        rgbVals = generateRGB(numSquares);
+        rgbVals = generateSquareColor(numSquares);
         h1.textContent = rgbVals[getRandomInt(rgbVals.length)];
-
+        generateSquares(numSquares);
+        display.textContent = "";
     });
 }
 
-// let easyButton = document.querySelector("#easy");
-// easyButton.addEventListener("click", function() {
-//     rgbVals = generateRGB(3);
-//     h1.textContent = rgbVals[getRandomInt(rgbVals.length)];
-//     generateSquares();
-//     for(let i=  3; i <=5; i++) {
-//         squares[i].style.background="#343a40";
-//     }
-//     difficulty = "easy";
-//     display.textContent = "";
-// });
-
-// let hardButton = document.querySelector("#hard");
-// hardButton.addEventListener("click", function() {
-//     rgbVals = generateRGB(6);
-//     h1.textContent = rgbVals[getRandomInt(rgbVals.length)];
-//     generateSquares();
-//     difficulty = "hard";
-//     display.textContent = "";
-// });
-
-let newButton = document.querySelector("#new");
-newButton.addEventListener("click", function() {
-    if(difficulty === "easy") {
-        easyDifficulty()
-    } else if(difficulty === "hard") {
-        hardDifficulty();
-    }
-    display.textContent = "";
+let resetButton = document.querySelector("#new");
+resetButton.addEventListener("click", function() {
+    reset();
 });
 
 
@@ -63,7 +42,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-function generateRGB(numColors) {
+function generateSquareColor(numColors) {
     let colors = [];
     for(let i = 0; i < numColors; i++) {
         let rgbString = "rgb(" + getRandomInt(256) + ", " + getRandomInt(256) + ", " + getRandomInt(256) + ")";
@@ -72,14 +51,11 @@ function generateRGB(numColors) {
     return colors;
 }
 
-
-function generateSquares() {
-    for(let i = 0; i < squares.length; i++) {
+function generateSquares(numSquares) {
+    for(let i = 0; i < 6; i++) {
         squares[i].style.backgroundColor = rgbVals[i];
     }
 }
-
-
 
 function makeChoice() {
     for(let i = 0; i < squares.length; i++) {
@@ -95,18 +71,9 @@ function makeChoice() {
     }
 }
 
-function mode() {
-
-}
-
-function hardDifficulty() {
-    rgbVals = generateRGB(6);
+function reset() {
+    rgbVals = generateSquareColor(numSquares);
     h1.textContent = rgbVals[getRandomInt(rgbVals.length)];
-    generateSquares();
-}
-
-function easyDifficulty() {
-    rgbVals = generateRGB(3);
-    h1.textContent = rgbVals[getRandomInt(rgbVals.length)];
-    generateSquares();
+    generateSquares(numSquares);
+    display.textContent = "";
 }
